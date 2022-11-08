06738dac0a07f15b314baf621fcbdda82cd9a64b
Francesca Santoriello <f.santoriello@hotmail.com>
2022-11-08 12:55:37 +0100

    ex3

--- 03-My-First-Event-Listener/lib/listener.js
+++ 03-My-First-Event-Listener/lib/listener.js
@@ -1,1 +1,8 @@
-// TODO: React to a click on the button!
+const button = document.getElementById('clickme');
+const audio = new Audio('sound.mp3');
+
+button.addEventListener('click', (e) => {
+  button.classList.add('disabled');
+  button.innerHTML = 'Bingo!';
+  audio.play();
+});
